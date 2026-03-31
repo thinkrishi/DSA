7c0fe3076f8e29fefe40bdca17867489ea89bd2d
thinkrishi <your-github-email>
2026-03-31 21:40:37 +0530

    update

---
+++ basics_01.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n = 9;
+    int m;
+
+    cout << n;
+    cin >> m;
+    cout << m;
+    return 0;
+
+}
+
+
+// long = 8 bytes
+// int = 4 bytes
+// short = 2 bytes
+// char = 1 bytegit
+// float = 4 bytes
+// double = 8 bytes
+// long double = 16 bytes
+// boolean = 1 byte its either 0 or 1  -------> 0= false and 1=true
+
+
+
