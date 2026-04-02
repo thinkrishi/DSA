eab2dc4e643defd377523c89234328829d1a1d70
rishikeshgowdakk <160719994+rishikeshgowdakk@users.noreply.github.com>
2026-04-03 00:25:37 +0530

    Initial DSA commit

---
+++ basics.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    int n;
+    float r;
+    cin >> r;
+
+    cout << r << endl;
+    cin.ignore();
+    getline(cin,s);
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        cout << s << endl;
+    }
+    return 0;
+}
