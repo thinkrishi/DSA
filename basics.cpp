9c5f8493becda5fb8599729a5cb4647039196eaf
rishikesh <rishikeshkk123@gmail.com>
2026-04-03 17:06:02 +0530

    learning

--- basics.cpp
+++ basics.cpp
@@ -1,3 +1,5 @@
+//BASIC PROGRAM 
+/*
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -6,3 +8,80 @@ int main(){
     cout << n;
     return 0;
 }
+*/
+//dataTYPES
+
+//primitve datatypes 
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    float w ;
+    long g;
+    double k;
+    char c;
+
+    string str;
+    
+    cin >> n ;
+    cout << n << endl;
+    cin >> w ;
+    cout << w << endl ;
+    cin >> g;
+    cout << g << endl;
+    cin >> k;
+    cout << k << endl;
+    cin >> c;
+    cout << c << endl;
+    cin >> str;
+    cout << str << endl;
+    return 0;
+}
+*/
+//if else statement
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >>  n;
+    if (n>10){
+        cout << "rishi";
+    }
+        else  cout << "king";
+        return 0;
+    }
+
+*/
+//if else if statement
+//lets say marks distribution
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    
+    cout << " enter the marks : " ;2
+    cin >> n;
+    
+
+    if(n>35){
+        cout << "pass";
+    }
+    else if (n>=35){
+        cout << "average";
+    }
+    else  cout << "fail";
+  cout << endl;
+  return 0;
+}
+  */
+
+  
+        
+
+
+    
+
+
