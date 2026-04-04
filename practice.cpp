6e8d68e052b08a75f71f96b53f7b54d5e37db371
rishikesh <rishikeshkk123@gmail.com>
2026-04-05 03:13:13 +0530

    practice

---
+++ practice.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x;
+
+    vector<int> ris;
+    
+    while(cin >> x){
+        ris.push_back(x);
+    for (auto &i : ris){
+        cout << i << " " ;
+    }
+    return 0;
+}
+}
