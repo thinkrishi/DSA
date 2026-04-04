45b96f97853345fcff25df1a46f6c3303959f741
rishikesh <rishikeshkk123@gmail.com>
2026-04-05 04:17:43 +0530

    update

--- practice.cpp
+++ practice.cpp
@@ -1,14 +1,60 @@
+//vetor input type 1
+
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int n;
-    cin >> n;
+    vector<int> a;
+    int x;
 
-    vector<int> a(n);
-    for(int &x : a) cin >> x;
+    while (cin >> x) {
+        a.push_back(x);
+    }
 
     for(int x : a) cout << x << " ";
 
+    return 0;
+}
+    */
+
+
+
+    //type two normal
+    /*
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+*/
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    vector < int > c(n);
+    for(auto &x : c) {
+        cin >> x ;
+    }
+    for(auto x : c){
+        cout << c << " ";
+    }
     return 0;
 }
